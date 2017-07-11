9f7953de53b87e5945e78d8a82ab45f88ac47db4
Jeffrey Scott <jeffrey.l.scott@nga.mil>
2017-07-11 18:47:25 -0400

    Added SQL quries to build database + tables

---
+++ queries/conduit-db.sql
@@ -0,0 +1,12 @@
+﻿-- Database: "conduit-db"
+
+-- DROP DATABASE "conduit-db";
+
+CREATE DATABASE "conduit-db"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'English_United States.1252'
+       LC_CTYPE = 'English_United States.1252'
+       CONNECTION LIMIT = -1;
+
---
+++ queries/users.sql
@@ -0,0 +1,17 @@
+﻿-- Table: "conduit-db".users
+
+-- DROP TABLE "conduit-db".users;
+
+CREATE TABLE "conduit-db".users
+(
+  id serial NOT NULL,
+  name_first character varying(64),
+  name_last character varying(64),
+  name_preferred character varying(64),
+  CONSTRAINT user_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "conduit-db".users
+  OWNER TO postgres;
