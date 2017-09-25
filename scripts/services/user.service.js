4806ad3c308ad697f062e7589bd8cf6bc7b6cf38
Jeffrey Scott <jeffrey.l.scott@nga.mil>
2017-09-25 19:20:19 -0400

    No longer breaks if auth code not found.

--- scripts/services/user.service.js
+++ scripts/services/user.service.js
@@ -2,15 +2,17 @@
 angular.module('conduit.services').factory('UserService', function($http, $window) { 
 	
 	var query = $window.location.search.substring(1);
-	var params = JSON.parse('{"' + decodeURI(query)
-									.replace(/"/g, '\\"')
-									.replace(/&/g, '","')
-									.replace(/=/g, '":"')
-							+ '"}');
-
-	var user = $http.get('/userInfo?code=' + params.code).then(function(response) {
-			return response.data;
-	});
+	if(query)
+		var params = JSON.parse('{"' + decodeURI(query)
+										.replace(/"/g, '\\"')
+										.replace(/&/g, '","')
+										.replace(/=/g, '":"')
+								+ '"}');
+	if(params && params.code)
+		var user = $http.get('/userInfo?code=' + params.code).then(function(response) {
+				return response.data;
+		});
+	else user = Promise.reject('No auth code');
 	
     var getUser = function() {
 	    return user;
