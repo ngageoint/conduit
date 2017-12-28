75ad98e14d2d52004be0faa685d3c70c0e768da9
Jeffrey Scott <jeffrey.l.scott@nga.mil>
2017-12-28 08:30:54 -0500

    rm node_modules

--- node_modules/docxtemplater-image-module/js/docUtils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var DocUtils = require("docxtemplater").DocUtils;
-DocUtils.convertPixelsToEmus = function (pixel) {
-	return Math.round(pixel * 9525);
-};
-module.exports = DocUtils;
