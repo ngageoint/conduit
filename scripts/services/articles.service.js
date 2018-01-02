9941a5e2aed7156cf714895558037e01e5b6b993
Scott.Jeffrey.1076746628 <jeffrey.l.scott@nga.mil>
2018-01-02 20:09:09 +0000

    Moved declarations before calls.

--- scripts/services/articles.service.js
+++ scripts/services/articles.service.js
@@ -13,6 +13,111 @@ ApiService, BooksService, DataSourceService, FilterService, RssLiteService, Arra
 			})
 		);
 
+        //Format rss sources into the accepted Conduit JSON format
+	var formatRss = function(feed, source)
+	{
+		var articles = [];
+		for(var i = 0; i < feed.length; i++)
+		{
+			var temp = {};
+			for(var j = 0; j < source.binding.length; j++)
+				temp[source.binding[j].local] = feed[i][source.binding[j].source];
+			temp.source = source.name
+			articles.push(forceArticleCompliance(temp));
+		}
+
+			for(var i = 0; i < articles.length; i++)
+			{
+				articles[i].tags.push(source.tag);
+				for(var j = 0; j < source.tags.length; j++)
+					if(source.tags[j])
+						articles[i].tags.push(ComplexPropertyTools.getComplexProperty(articles[i], source.tags[j]));
+			}
+			return articles;
+	}
+	
+	var completeCompliance = function (article, books) {
+		//Set booleans
+		if(typeof article.active == "undefined")
+			article.active = false;
+		if(typeof article.activeInBook == "undefined")
+			article.activeInBook = false;
+		if(typeof article.build == "undefined")
+			article.build = false;
+		if(typeof article.inBook == "undefined")
+			article.inBook = false;
+		if(typeof article.inFeed == "undefined")
+			article.inFeed = true;
+		if(typeof article.isEdit == "undefined")
+			article.isEdit = false;
+		if(typeof article.read == "undefined")
+			article.read = false;
+
+		//Set fields created by conduit
+		if(typeof article.books == "undefined")
+			article.books = [];
+		if(typeof article.edits == "undefined")
+			article.edits = [];
+			
+		//Ensure articles with existing books are referencing the correct object
+		if(article.books.length > 0 && books) {
+			for(var i = 0; i < article.books.length; i++) {
+				for(var j = 0; j < books.length; j++) {
+					if(article.books[i].id === books[j].id)
+						article.books[i] = books[j]
+				}
+			}
+		}
+		if(typeof article.comments == "undefined")
+			article.comments = [];
+
+		//Set additional fields required by conduit
+		if(typeof article.date == "undefined")
+			article.date = new Date();
+		if(typeof article.selectedImage == "undefined")
+			article.selectedImage = 0;
+		if(typeof article.tags == "undefined")
+			article.tags = [];
+
+		return article;
+	}
+
+//Force the article to have the minimum fields and set defaults if none given
+	var forceArticleCompliance = function(article, books) {
+		//return new Promise(function(resolve, reject) {
+
+			//Set hashable values first		
+			if(typeof article.title == "undefined")
+				article.title = "";
+			if(typeof article.text == "undefined")
+				article.text = "";
+			if(typeof article.images == "undefined")
+				article.images = [];
+			if(typeof article.source == "undefined")
+				article.source = "";
+
+			if(!article.id || article.id === "") {
+				ApiService.generateHash(article).then(function(hash) {
+					article.id = hash
+					return completeCompliance(article, books);
+				}).catch(function(err) {
+					article.id = generateUUID();
+					return completeCompliance(article, books);
+				});
+			} else {
+				return completeCompliance(article, books);
+			}	
+		//});
+	}
+
+	//RFC4122 version 4 compliant; the chance of a collision is less than 1 in 2.71 quintillion
+	var generateUUID = function() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+				var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+				return v.toString(16);
+		});
+	}
+
 		//Request all promises from our queries array, concat them, and return
 		return $q.all(queries).then(function(results) {
 			var fullResponse = [];
@@ -75,111 +180,6 @@ ApiService, BooksService, DataSourceService, FilterService, RssLiteService, Arra
   	var getArticles = function() {
 		return articles;
 	};
-
-	//Format rss sources into the accepted Conduit JSON format
-	var formatRss = function(feed, source)
-	{
-		var articles = [];
-		for(var i = 0; i < feed.length; i++)
-		{
-			var temp = {};
-			for(var j = 0; j < source.binding.length; j++)
-				temp[source.binding[j].local] = feed[i][source.binding[j].source];
-			temp.source = source.name
-			articles.push(forceArticleCompliance(temp));
-		}
-
-			for(var i = 0; i < articles.length; i++)
-			{
-				articles[i].tags.push(source.tag);
-				for(var j = 0; j < source.tags.length; j++)
-					if(source.tags[j])
-						articles[i].tags.push(ComplexPropertyTools.getComplexProperty(articles[i], source.tags[j]));
-			}
-			return articles;
-	}
-
-//Force the article to have the minimum fields and set defaults if none given
-	var forceArticleCompliance = function(article, books) {
-		//return new Promise(function(resolve, reject) {
-
-			//Set hashable values first		
-			if(typeof article.title == "undefined")
-				article.title = "";
-			if(typeof article.text == "undefined")
-				article.text = "";
-			if(typeof article.images == "undefined")
-				article.images = [];
-			if(typeof article.source == "undefined")
-				article.source = "";
-
-			if(!article.id || article.id === "") {
-				ApiService.generateHash(article).then(function(hash) {
-					article.id = hash
-					return completeCompliance(article, books);
-				}).catch(function(err) {
-					article.id = generateUUID();
-					return completeCompliance(article, books);
-				});
-			} else {
-				return completeCompliance(article, books);
-			}	
-		//});
-	}
-
-	var completeCompliance = function (article, books) {
-		//Set booleans
-		if(typeof article.active == "undefined")
-			article.active = false;
-		if(typeof article.activeInBook == "undefined")
-			article.activeInBook = false;
-		if(typeof article.build == "undefined")
-			article.build = false;
-		if(typeof article.inBook == "undefined")
-			article.inBook = false;
-		if(typeof article.inFeed == "undefined")
-			article.inFeed = true;
-		if(typeof article.isEdit == "undefined")
-			article.isEdit = false;
-		if(typeof article.read == "undefined")
-			article.read = false;
-
-		//Set fields created by conduit
-		if(typeof article.books == "undefined")
-			article.books = [];
-		if(typeof article.edits == "undefined")
-			article.edits = [];
-			
-		//Ensure articles with existing books are referencing the correct object
-		if(article.books.length > 0 && books) {
-			for(var i = 0; i < article.books.length; i++) {
-				for(var j = 0; j < books.length; j++) {
-					if(article.books[i].id === books[j].id)
-						article.books[i] = books[j]
-				}
-			}
-		}
-		if(typeof article.comments == "undefined")
-			article.comments = [];
-
-		//Set additional fields required by conduit
-		if(typeof article.date == "undefined")
-			article.date = new Date();
-		if(typeof article.selectedImage == "undefined")
-			article.selectedImage = 0;
-		if(typeof article.tags == "undefined")
-			article.tags = [];
-
-		return article;
-	}
-
-	//RFC4122 version 4 compliant; the chance of a collision is less than 1 in 2.71 quintillion
-	var generateUUID = function() {
-		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-				var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-				return v.toString(16);
-		});
-	}
 	
 	return {
 	  	getArticles: getArticles,
