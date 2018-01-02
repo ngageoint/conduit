4ba348b07089b9667bf697157fe5de89371ef9ba
Scott.Jeffrey.1076746628 <jeffrey.l.scott@nga.mil>
2018-01-02 19:53:32 +0000

    Moved catch up to axios (instead of promise).

--- server/article-reader/rss-lite.service.js
+++ server/article-reader/rss-lite.service.js
@@ -10,51 +10,50 @@ module.exports = {
     
             axios.get(url, config).then(function(response) { 
                 //return new Promise(function(resolve, reject) {
-                    var parser = new xml2js.Parser({ignoreAttrs : false, mergeAttrs : true})
-                    var parseString = parser.parseString;
-                    var xml = response.data
+                var parser = new xml2js.Parser({ignoreAttrs : false, mergeAttrs : true})
+                var parseString = parser.parseString;
+                var xml = response.data
 
-                    parseString(xml, function (err, res) {
-                        xml = res;
-                    });
-                    var entries;
-                    var parsed = [];
-
-                    //Atom 1.0 support
-                    if(xml.feed) {
-                        entries = xml.feed.entry;
-                    }
-                    //RSS 2.0 support
-                    else {
-                        entries = xml.channel.item;
-                    }
-
-                    for(var i = 0; i < entries.length; i++) {
-                        var temp = {};
-                        var keys = Object.keys(entries[i]);
-
-                        for(var j = 0; j < keys.length; j++) {
-                            if(keys[j] === "link") {
-                                if(entries[i].link[0].href) {
-                                    temp.link = entries[i].link[0].href.toString()
-                                }
-                            } else {
-                                temp[keys[j]] = entries[i][keys[j]][0];
-                            }
-                        }
-                        parsed.push(temp);
-                    } 
-
-                    if(parsed) {
-                        return resolve(parsed);
-                    } else {
-                        return reject();
-                    }
+                parseString(xml, function (err, res) {
+                    xml = res;
                 });
+                var entries;
+                var parsed = [];
+
+                //Atom 1.0 support
+                if(xml.feed) {
+                    entries = xml.feed.entry;
+                }
+                //RSS 2.0 support
+                else {
+                    entries = xml.channel.item;
+                }
+
+                for(var i = 0; i < entries.length; i++) {
+                    var temp = {};
+                    var keys = Object.keys(entries[i]);
+
+                    for(var j = 0; j < keys.length; j++) {
+                        if(keys[j] === "link") {
+                            if(entries[i].link[0].href) {
+                                temp.link = entries[i].link[0].href.toString()
+                            }
+                        } else {
+                            temp[keys[j]] = entries[i][keys[j]][0];
+                        }
+                    }
+                    parsed.push(temp);
+                }
+
+                if(parsed) {
+                    return resolve(parsed);
+                } else {
+                    return reject();
+                }
             }).catch(function(err) {
                 console.error(err);
                 return reject(err);
-            });                    
-        //});
+            })
+        });                    
 	}
 }
