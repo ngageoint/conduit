ae41a96e2e0304154e1a56776a3695eaaf993fbf
Jeffrey Scott <jeffrey.l.scott@nga.mil>
2018-01-11 10:32:16 -0500

    scrollCenterOn directive

--- scripts/directives/scrollCenterOn.directive.js
+++ scripts/directives/scrollCenterOn.directive.js
@@ -4,22 +4,22 @@
  * <div data-scroll-to-top="selectedIndex"></div>
  */
 angular.module('conduit.directives').directive('scrollCenterOn', function() {
-		return {
-			restrict: 'A',
-			link: function (scope, elem, attrs) {
-				console.log('scroll center on created, default ' + attrs.scrollCenterOn)
-				scope.$watch(attrs.scrollCenterOn, function(newVal, oldVal) {
-					console.log(newVal);
-					if(newVal) {
-						let parent = elem.parent()[0];
-						console.log(parent.scrollTop);
-						console.log(elem[0].offsetTop);
+	return {
+		restrict: 'A',
+		link: function (scope, elem, attrs) {
+			console.log('scroll center on created, default ' + attrs.scrollCenterOn)
+			scope.$watch(attrs.scrollCenterOn, function(newVal, oldVal) {
+				console.log(newVal);
+				if(newVal) {
+					let parent = elem.parent()[0];
+					console.log(parent.scrollTop);
+					console.log(elem[0].offsetTop);
 
-						let scrollTop = elem[0].offsetTop;
+					let scrollTop = elem[0].offsetTop;
 
-						console.log(parent.scrollTop);
-					}
-				});
-			}
-		};
-	});
+					console.log(parent.scrollTop);
+				}
+			});
+		}
+	};
+});
