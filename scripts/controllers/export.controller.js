6dfb67f88d7881df6fb7f1234d66b8a0ec4a7438
Jeffrey Scott <scott@Jeffreys-MacBook-Pro.local>
2018-01-25 11:45:13 -0500

    Removed export ctrl from front end

--- scripts/controllers/export.controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('conduit.controllers').controller('ExportCtrl', function($q, $scope, ApiService, KeyboardService, DateTools, XMLTools, __config) {
-	/**
-	 * This function exports a given article into a .docx file based on the template provided in Conduit
-	 * 
-	 * @param {object} article An article following the minimum Conduit format
-	 */
-	
-});
