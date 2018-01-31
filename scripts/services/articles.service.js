04e9ace5d4417f098514e25df6ec767bfbe7de60
scottjl@Corp.ad-test-2012.com <scottjl@Corp.ad-test-2012.com>
2018-01-31 08:44:12 -0500

    reduce tech debt on articles.service

--- scripts/services/articles.service.js
+++ scripts/services/articles.service.js
@@ -1,44 +1,6 @@
 /* The ArticlesService makes all of the articles available in a global, editable promise. */
 angular.module('conduit.services').factory('ArticlesService', function($q, $http, $timeout,
-ApiService, BooksService, DataSourceService, FilterService, ArrayTools, ComplexPropertyTools, DateTools, __config) { 
-
-	var articles = 	DataSourceService.getSources().then(function(sources) {
-		
-		var queries = [];
-
-		queries.push(ApiService.select.articleBlock(DateTools.formatDate(new Date(new Date().setDate(new Date().getDate() - __config.MAX_DAYS_BACK)), 'yyyy-MM-dd'), 0).then(function(response) {
-				return response.articles;
-			})
-		);
-		
-		//Request all promises from our queries array, concat them, and return
-		return $q.all(queries).then(function(results) {
-			var fullResponse = [];
-			for(var i = 0; i < results.length; i++) {
-				fullResponse = fullResponse.concat(results[i]);
-			}
-
-			return BooksService.getBooks().then(function(books) {
-				var articles = []
-				for(var i = 0; i < fullResponse.length; i++) {
-					(function(article) {
-						articles.push(forceArticleCompliance(article, books));
-					})(fullResponse[i]);
-				}
-				return $q.all(articles).then(function(articles) {
-					//Send articles to db
-					//ApiService.insert.articleFull(articles);
-					articles = ArrayTools.removeDuplicates(articles, function(thisArticle) {
-						return thisArticle.id;
-					});
-
-					return articles;
-				});
-			}).catch(function(err) {
-				return fullResponse
-			});			
-		});			
-	});
+ApiService, BooksService, FilterService, ArrayTools, ComplexPropertyTools, DateTools, __config) { 
 	
 	var completeCompliance = function (article, books) {
 		//Set booleans
@@ -86,7 +48,7 @@ ApiService, BooksService, DataSourceService, FilterService, ArrayTools, ComplexP
 		return article;
 	}
 
-//Force the article to have the minimum fields and set defaults if none given
+	//Force the article to have the minimum fields and set defaults if none given
 	var forceArticleCompliance = function(article, books) {
 		//return new Promise(function(resolve, reject) {
 
@@ -104,7 +66,7 @@ ApiService, BooksService, DataSourceService, FilterService, ArrayTools, ComplexP
 				ApiService.generateHash(article).then(function(hash) {
 					article.id = hash
 					return completeCompliance(article, books);
-				}).catch(function(err) {
+				}).catch(function() {
 					article.id = generateUUID();
 					return completeCompliance(article, books);
 				});
@@ -121,6 +83,45 @@ ApiService, BooksService, DataSourceService, FilterService, ArrayTools, ComplexP
 				return v.toString(16);
 		});
 	}
+
+	var getAndProcessArticles = function() {
+		var queries = [];
+
+		queries.push(ApiService.select.articleBlock(DateTools.formatDate(new Date(new Date().setDate(new Date().getDate() - __config.MAX_DAYS_BACK)), 'yyyy-MM-dd'), 0).then(function(response) {
+				return response.articles;
+			})
+		);
+		
+		//Request all promises from our queries array, concat them, and return
+		return $q.all(queries).then(function(results) {
+			var fullResponse = [];
+			for(var i = 0; i < results.length; i++) {
+				fullResponse = fullResponse.concat(results[i]);
+			}
+
+			return BooksService.getBooks().then(function(books) {
+				var articles = []
+				for(var i = 0; i < fullResponse.length; i++) {
+					(function(article) {
+						articles.push(forceArticleCompliance(article, books));
+					})(fullResponse[i]);
+				}
+				return $q.all(articles).then(function(articles) {
+					//Send articles to db
+					//ApiService.insert.articleFull(articles);
+					articles = ArrayTools.removeDuplicates(articles, function(thisArticle) {
+						return thisArticle.id;
+					});
+
+					return articles;
+				});
+			}).catch(function() {
+				return fullResponse
+			});			
+		});
+	}
+	
+	var articles = getAndProcessArticles();
 	
   	var getArticles = function() {
 		return articles;
